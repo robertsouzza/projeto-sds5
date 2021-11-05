0657844cae4919501a77f94d19e9325f6b8ca70b
Roberto Souza <rcsouzza@gmail.com>
2021-11-04 22:13:58 -0400

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import DataTable from "compnents/DataTable";
+import Footer from "compnents/Footer";
+import NavBar from "compnents/NavBar";
 
 function App() {
   return (
-    <div >
-       <h1 className ="text-primary">Olá Mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá Mundo!</h1>
+
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
