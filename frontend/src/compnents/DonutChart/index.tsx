6b84827710988048385dc171f4dcb8e9ad80a6ff
Roberto Souza <rcsouzza@gmail.com>
2021-11-06 18:22:06 -0400

    First request

--- frontend/src/compnents/DonutChart/index.tsx
+++ frontend/src/compnents/DonutChart/index.tsx
@@ -1,10 +1,32 @@
+import axios from 'axios';
+import { type } from 'os';
 import Chart from 'react-apexcharts';
-const DonutChart = () => {
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
 
-    const mockData = {
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
+const DonutChart = () => {
+    
+    //froam errada
+    let chartData : ChartData = { labels: [], series: []};
+                                         // importando do backand operação asincrona.
+    axios.get(`${BASE_URL}/sales/amount-by-seller` ).then(response =>{
+            const data = response.data as SaleSum[]; 
+            const myLabels = data.map(x => x.sellerName); 
+            const mySeries = data.map(x => x.sum);
+
+            chartData = { labels: myLabels, series: mySeries};
+            console.log(chartData);
+          });                             
+    
+    // funcão para ser executada quando a resposta chegar
+    /*const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+    }*/
     
     const options = {
         legend: {
@@ -15,8 +37,8 @@ const DonutChart = () => {
 
     return (
         <Chart 
-             options={{...options, labels: mockData.labels}}
-             series={mockData.series}
+             options={{...options, labels: chartData.labels}}
+             series={chartData.series}
              type="donut"
              height="240"
         />
